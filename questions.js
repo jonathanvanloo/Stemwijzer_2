5ea2d43c13f0bdfee1521585a56290a766f83cab
jonathan <99052897@mydavinci.nl>
2021-02-16 10:27:05 +0100

    selecteren van een antwoord toegevoegd

--- questions.js
+++ questions.js
@@ -1,36 +1,37 @@
 const buttonBack = document.getElementById('back');
 const buttonNext = document.getElementById('next');
-const buttonTitle = document.getElementById('title'); 
-const buttonStatement = document.getElementById('statement'); 
-const buttonPro = document.getElementById('pro'); 
-const buttonNone = document.getElementById('none'); 
-const buttonContra = document.getElementById('contra'); 
+const buttonTitle = document.getElementById('title');
+const buttonStatement = document.getElementById('statement');
+const buttonPro = document.getElementById('pro');
+const buttonNone = document.getElementById('none');
+const buttonContra = document.getElementById('contra');
 var currentQuestion = 0;
 var answers = [];
+var answerd = false;
 var allParties = [
-{name: "VVD", score: 0},
-{name: "CDA", score: 0},
-{name: "PVV", score: 0},
-{name: "D66", score: 0},
-{name: "GroenLinks", score: 0},
-{name: "SP", score: 0},
-{name: "PvdA", score: 0},
-{name: "ChristenUnie", score: 0},
-{name: "Partij voor de Dieren", score: 0},
-{name: "SGP", score: 0},
-{name: "DENK", score: 0},
-{name: "Forum voor Democratie", score: 0},
-{name: "Lokaal in de Kamer", score: 0},
-{name: "OndernemersPartij", score: 0},
-{name: "VNL", score: 0},
-{name: "Nieuwe Wegen", score: 0},
-{name: "De Burger Beweging", score: 0},
-{name: "Piratenpartij", score: 0},
-{name: "Artikel 1", score: 0},
-{name: "Libertarische Partij", score: 0},
-{name: "50Plus", score: 0},
-{name: "Vrijzinnige Partij", score: 0},
-{name: "Niet Stemmers", score: 0}
+	{name: "VVD", score: 0},
+	{name: "CDA", score: 0},
+	{name: "PVV", score: 0},
+	{name: "D66", score: 0},
+	{name: "GroenLinks", score: 0},
+	{name: "SP", score: 0},
+	{name: "PvdA", score: 0},
+	{name: "ChristenUnie", score: 0},
+	{name: "Partij voor de Dieren", score: 0},
+	{name: "SGP", score: 0},
+	{name: "DENK", score: 0},
+	{name: "Forum voor Democratie", score: 0},
+	{name: "Lokaal in de Kamer", score: 0},
+	{name: "OndernemersPartij", score: 0},
+	{name: "VNL", score: 0},
+	{name: "Nieuwe Wegen", score: 0},
+	{name: "De Burger Beweging", score: 0},
+	{name: "Piratenpartij", score: 0},
+	{name: "Artikel 1", score: 0},
+	{name: "Libertarische Partij", score: 0},
+	{name: "50Plus", score: 0},
+	{name: "Vrijzinnige Partij", score: 0},
+	{name: "Niet Stemmers", score: 0}
 ];
 
 vraag()
@@ -44,7 +45,6 @@ function vraag() {
 	buttonNone.onclick = none;
 	buttonContra.onclick = contra;
 	prosessAnswer()
-	console.log(allParties)
 }
 
 function prosessAnswer() {
@@ -53,18 +53,26 @@ function prosessAnswer() {
 	for (var i = 0; i <= subj; i++) {;
 		for (var a = 0; a <= party; a++) {
 			if (subjects[i].parties[a].position == answers[currentQuestion-1]) {
-				return findPosition(subjects[i].parties[a].name, allParties)
+				return findPosition(subjects[i].parties[a].name, allParties);
 			}
 		}
 	}
-	console.log(allParties)
 }
 
 function findPosition(key, array) {
 	for (var i = 0; i < array.length; i++) {
 		if (array[i].name == key) {
-			array[i].score++
+			array[i].score++;
 		}
+	}
+}
+
+function selectedAnswer(button) {
+	if (answers[currentQuestion-1].length == currentQuestion) {
+		answerd = true
+	}
+	if (answerd) {
+		button.style.backgroundColor = "deepskyblue";
 	}
 }
 
@@ -87,17 +95,20 @@ function next() {
 }
 
 function pro() {
-	answers[currentQuestion] = 'pro';
+	answers[currentQuestion] = 'Pro';
+	selectedAnswer(buttonPro)
 	next();
 }
 
 function none() {
-	answers[currentQuestion] = 'none';
+	answers[currentQuestion] = 'None';
+	selectedAnswer(buttonNone)
 	next();
 }
 
 function contra() {
-	answers[currentQuestion] = 'contra';
+	answers[currentQuestion] = 'Contra';
+	selectedAnswer(buttonContra)
 	next();
 }
 
